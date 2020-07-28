c862766e70ecf7751bef2ff50b855b6fb364f7e3
fillerInk <thatonephysicsnerd@gmail.com>
2020-07-28 23:24:38 +0530

    🐦 updation!

--- twitter-name-update/index.js
+++ twitter-name-update/index.js
@@ -1,9 +1,16 @@
-// install express with `npm install express` 
 const express = require('express')
-const app = express()
+const twitter = require('twitter')
 require('dotenv').config()
 
-const twitter = require('twitter')
+const { App } = require("deta");
+const app = App(express());
+
+const client = new twitter({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key ,
+    access_token_secret: process.env.access_token_secret
+})  
 
 const digits = {
     0 : '0️⃣',
@@ -18,13 +25,6 @@ const digits = {
     9 : '9️⃣',
 }
 
-const client = new twitter({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token_key: process.env.access_token_key ,
-    access_token_secret: process.env.access_token_secret
-})
-
 function emojify(number){
     let result = ''
     number = number.toString()
@@ -34,27 +34,32 @@ function emojify(number){
     return result
 }
 
-
-
-client.get('account/verify_credentials',(error,response)=>{
-    if(!error){
-        const followerCount = response.followers_count
-        const params = { name: `Sreeram is @ ${emojify(followerCount)} followers!`}
-        client.post('account/update_profile',params,(error,response)=>{
-            if(error) throw error
-            console.log('Updated')
-        })
-    }
-})
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-const PORT = 3000
-
-if(!process.env.DETA_RUNTIME){
-    app.listen(PORT,()=>{
-        console.log('Listening to local port')
+function twitterUpdate(){
+    client.get('account/verify_credentials',(error,response)=>{
+        if(!error){
+            const followerCount = response.followers_count
+            const params = { name: `Sreeram @ ${emojify(followerCount)}`}
+            client.post('account/update_profile',params,(error,response)=>{
+                if(error) throw error
+                console.log(error)
+                console.log('Updated')
+            })
+        }
     })
 }
-// export 'app'
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.lib.cron(event => {
+    twitterUpdate()      
+});
+
+// For testing locally
+// const PORT = 3000
+
+// if(!process.env.DETA_RUNTIME){
+//     app.listen(PORT,()=>{
+//         console.log('Listening to local port')
+//     })
+// }    
 module.exports = app
